2733302b23227cdd468eac270fb64ce3701dbf80
vedanshu1234 <chaudharyvedanshu61@gmail.com>
2025-01-01 14:44:25 +0530

    Update info.py

--- info.py
+++ info.py
@@ -19,7 +19,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', '7795485488:AAF-JYvxVr04HRt0mIXg4bXAi4LeNOa
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '-1001944607209').split()]
-USERNAME = environ.get('USERNAME', "https://t.me/Innocent_babe_dead") # ADMIN USERNAME
+USERNAME = environ.get('USERNAME', "https://t.me/Innocent_babe_dead") veer_1_2
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002466739370'))
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/+K2kgCBgaat80YWQ9')
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002064987723').split()]
